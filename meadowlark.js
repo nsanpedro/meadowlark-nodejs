ad12e0f3239090a775db1371c605c92e69c66a55
nicosp <nicolassanpedro@gmail.com>
2018-04-02 15:38:19 -0300

    Module fortune cookie

--- meadowlark.js
+++ meadowlark.js
@@ -1,12 +1,9 @@
 var express = require('express');
+var fortune = require('./lib/fortune.js');
 
 var app = express();
 
-var fortunes = [
-    "Conquer your fears or they will conquer you.", "Rivers need springs.",
-    "Do not fear what you don't know.",
-    "You will have a pleasant surprise.", "Whenever possible, keep it simple.",
-];
+
 
 var handlebars = require('express3-handlebars').create({
     defaultLayout: 'main'
@@ -27,8 +24,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/about', function (req, res) {
-    var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
-    res.render('about', { fortune: randomFortune });
+    res.render('about', { fortune: fortune.getFortune() });
 });
 
 app.use(function (req, res, next) {
