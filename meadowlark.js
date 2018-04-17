fe9bbeddb78926127b884b255c35f058a7b97d43
nicosp <nicolassanpedro@gmail.com>
2018-04-16 22:49:41 -0300

    handlebars & testing

--- meadowlark.js
+++ meadowlark.js
@@ -18,14 +18,34 @@ app.set('port', process.env.PORT || 3000);
 app.use(express.static(__dirname + '/public'));
 
 
-////Get for home/about/notfount/server error
+///// test querystring
+app.use(function (req, res, next) {
+    res.locals.showTests = app.get('env') !== 'production' &&
+        req.query.test === '1';
+    next();
+});
+
+
+////ROUTES Get for home/about/notfount/server error
 app.get('/', function (req, res) {
     res.render('home');
 });
 
 app.get('/about', function (req, res) {
-    res.render('about', { fortune: fortune.getFortune() });
+    res.render('about', {
+        fortune: fortune.getFortune(),
+        pageTestScript: '/qa/tests-about.js'
+    });
 });
+
+app.get('/tours/hood-river', function(req, res) {
+   res.render('tours/hood-river'); 
+});
+
+app.get('/tours/request-group-rate', function(req, res) {
+   res.render('tours/request-group-rate'); 
+});
+
 
 app.use(function (req, res, next) {
     res.status(404);
