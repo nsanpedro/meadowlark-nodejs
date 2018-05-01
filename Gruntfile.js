8686d31b09c1c6cedc9eea1c53034298976a70a4
nicosp <nicolassanpedro@gmail.com>
2018-05-01 20:03:01 -0300

    unitTest & grunt

---
+++ Gruntfile.js
@@ -0,0 +1,30 @@
+module.exports = function(grunt) {
+  // load plugins
+  ['grunt-cafe-mocha', 'grunt-contrib-jshint', 'grunt-exec'].forEach(function(task) {
+    grunt.loadNpmTasks(task);
+  });
+
+  grunt.initConfig({
+    cafemocha: {
+      all: {
+        src: 'qa/tests-*.js',
+        options: {
+          ui: 'tdd'
+        }
+      }
+    },
+    jshint: {
+      app: [
+        'meadowlark.js', 'public/js/**/*.js', 'lib/**/*.js'
+      ],
+      qa: ['Gruntfile.js', 'public/qa/**/*.js', 'qa/**/*.js'],
+    },
+    exec: {
+      linkchecker: { cmd: 'linkchecker http://localhost:3000' }
+    },
+  });
+
+  // register tasks
+  grunt.registerTask('default', ['cafemocha','jshint','exec']);
+
+};
--- meadowlark.js
+++ meadowlark.js
@@ -39,11 +39,11 @@ app.get('/about', function (req, res) {
 });
 
 app.get('/tours/hood-river', function(req, res) {
-   res.render('tours/hood-river'); 
+   res.render('tours/hood-river');
 });
 
 app.get('/tours/request-group-rate', function(req, res) {
-   res.render('tours/request-group-rate'); 
+   res.render('tours/request-group-rate');
 });
 
 
